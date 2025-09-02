bf72b65b8287f18abbd72c045361e33f0a072e60
Justin-cpubeep <jthwise@gmail.com>
2025-09-02 09:52:34 +0900

    First upload

---
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -0,0 +1,15 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @GetMapping("/sbb")
+    @ResponseBody
+    public String index() {
+        System.out.println("알자드 고온!!!");
+        return "만두만두 군만9999두";
+    }
+}
